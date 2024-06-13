bb3d815ff66a75a88d5d3f0d1e838fba277b6c09
jiangtao <jiangtao.cctv@qq.com>
2024-06-13 17:47:07 +0800

    调整配置密钥方式

--- replace.sh
+++ replace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "第一个参数是: $1"
-sed -i "s/REPLACE_QL_URL/${QL_URL}/g" app/src/main/java/cn/moon/ql/Config.java
-sed -i "s/REPLACE_QL_CLIENT_ID/${QL_CLIENT_ID}/g" app/src/main/java/cn/moon/ql/Config.java
-sed -i "s/REPLACE_QL_CLIENT_SECRET/${QL_CLIENT_SECRET}/g" app/src/main/java/cn/moon/ql/Config.java
+sed -i "s/REPLACE_QL_URL/$1/g" app/src/main/java/cn/moon/ql/Config.java
+sed -i "s/REPLACE_QL_CLIENT_ID/$2/g" app/src/main/java/cn/moon/ql/Config.java
+sed -i "s/REPLACE_QL_CLIENT_SECRET/$3/g" app/src/main/java/cn/moon/ql/Config.java
 cat app/src/main/java/cn/moon/ql/Config.java
